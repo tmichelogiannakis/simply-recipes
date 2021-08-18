70b7dfb532d140620284317ce3a42c2984ed1553
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-08-19 01:05:09 +0300

    feat: add a layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const Layout: FC = ({ children }) => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col min-h-screen">
+        <div className="flex-initial order-2 flex-grow">{children}</div>
+        <div className="flex-initial order-1">
+          <Navbar />
+        </div>
+        <div className="flex-initial order-3">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
