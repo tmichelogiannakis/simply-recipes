1bcc22bbc02f55785530d890be1003b326cdf4cf
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-08-19 01:00:57 +0300

    feat: create a Navbar component

---
+++ src/components/Layout/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Link } from 'gatsby';
+
+const Navbar = (): JSX.Element => {
+  return (
+    <nav>
+      <h2 className="overflow-hidden whitespace-nowrap indent-full h-0">
+        Navigation menu
+      </h2>
+      <div className="flex items-center py-4">
+        <div className="mr-8">
+          <Link to="/">
+            <span className="sr-only">Simply recipes home page</span>
+            <img src="logo.svg" alt="simply recipes" />
+          </Link>
+        </div>
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
