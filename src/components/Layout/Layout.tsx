c39a0cab9d31f849cfeed29532b1fbfd19fcb089
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-08-19 18:37:12 +0300

    feat: style not found page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,7 +5,9 @@ import Navbar from './Navbar';
 const Layout: FC = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-initial order-2 flex-grow">{children}</div>
+      <div className="flex-initial order-2 flex-grow">
+        <div className="container mx-auto px-4">{children}</div>
+      </div>
       <div className="flex-initial order-1">
         <Navbar />
       </div>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,8 +3,9 @@ import Layout from '../components/Layout/Layout';
 const NotFound = (): JSX.Element => {
   return (
     <Layout>
-      <div className="container mx-auto">
-        <h1>Not Found!</h1>
+      <div className="text-center">
+        <p className="text-9xl leading-snug">404</p>
+        <h1 className="text-3xl leading-snug">Page Not Found</h1>
       </div>
     </Layout>
   );
