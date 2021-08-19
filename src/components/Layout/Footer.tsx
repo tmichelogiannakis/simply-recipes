4ce8ff0023907b1adabbc2d81649e8bcd9a4f8b8
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-08-19 18:27:51 +0300

    feat: style footer

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,6 +1,10 @@
 const Footer = (): JSX.Element => {
   const year = new Date().getFullYear();
-  return <footer>© {year} SimplyRecipes</footer>;
+  return (
+    <footer className="py-2 bg-black text-white">
+      <div className="container px-4 mx-auto">&copy; {year} SimplyRecipes</div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,15 +4,13 @@ import Navbar from './Navbar';
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="container mx-auto">
-      <div className="flex flex-col min-h-screen">
-        <div className="flex-initial order-2 flex-grow">{children}</div>
-        <div className="flex-initial order-1">
-          <Navbar />
-        </div>
-        <div className="flex-initial order-3">
-          <Footer />
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-initial order-2 flex-grow">{children}</div>
+      <div className="flex-initial order-1">
+        <Navbar />
+      </div>
+      <div className="flex-initial order-3">
+        <Footer />
       </div>
     </div>
   );
