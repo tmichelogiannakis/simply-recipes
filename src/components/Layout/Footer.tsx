11faf5c9a71d54cf46dcdf3e3e9f0c6a1926cec7
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2021-08-20 11:13:36 +0300

    feat: use

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,7 +2,9 @@ const Footer = (): JSX.Element => {
   const year = new Date().getFullYear();
   return (
     <footer className="py-2 bg-black text-white">
-      <div className="container px-4 mx-auto">&copy; {year} SimplyRecipes</div>
+      <div className="max-w-screen-xl px-4 mx-auto">
+        &copy; {year} SimplyRecipes
+      </div>
     </footer>
   );
 };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,11 @@ import { FC } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
-const Layout: FC = ({ children }) => {
+const Layout: FC = ({ children }): JSX.Element => {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-initial order-2 flex-grow">
-        <div className="container mx-auto px-4">{children}</div>
+        <div className="max-w-screen-xl mx-auto px-4">{children}</div>
       </div>
       <div className="flex-initial order-1">
         <Navbar />
